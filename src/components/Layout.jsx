1425cc248e79019cf81a6f204bcf81f9859ac4c5
Parmeet Singh <psparmeet14@gmail.com>
2025-12-06 13:23:54 +0530

    feat: Implement scroll-to-top functionality on route change in Layout component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,20 +1,29 @@
-import { Outlet } from 'react-router-dom'
+import { useEffect } from 'react'
+import { Outlet, useLocation } from 'react-router-dom'
 import Footer from './Footer'
 import Navbar from './Navbar'
 
-const Layout = () => (
-  <div className="app-shell">
-    <aside className="app-sidebar">
-      <Navbar />
-    </aside>
+const Layout = () => {
+  const location = useLocation()
 
-    <div className="app-main">
-      <div className="app-content">
-        <Outlet />
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'auto' })
+  }, [location.pathname])
+
+  return (
+    <div className="app-shell">
+      <aside className="app-sidebar">
+        <Navbar />
+      </aside>
+
+      <div className="app-main">
+        <div className="app-content">
+          <Outlet />
+        </div>
+        <Footer />
       </div>
-      <Footer />
     </div>
-  </div>
-)
+  )
+}
 
 export default Layout
